fabef485c5f85196c8475207e78e0812e032d02d
EuphoriaX2 <99680795+EuphoriaX2@users.noreply.github.com>
2022-10-29 21:34:47 -0500

    Add files via upload

---
+++ card generator.lua
@@ -0,0 +1,53 @@
+function generateHand()
+  card1 = math.random(1, 4)
+  card2 = math.random(1, 4)
+  card3 = math.random(1, 4)
+  card4 = math.random(1, 4)
+  card5 = math.random(1, 4)
+  if card1 == 1 then
+    print("spade")
+  elseif card1 == 2 then
+    print("club")
+  elseif card1 == 3 then
+    print("diamond")
+  elseif card1 == 4 then
+    print("heart")
+  end
+  if card2 == 1 then
+    print("spade")
+  elseif card2 == 2 then
+    print("club")
+  elseif card2 == 3 then
+    print("diamond")
+  elseif card2 == 4 then
+    print("heart")
+  end
+  if card3 == 1 then
+    print("spade")
+  elseif card3 == 2 then
+    print("club")
+  elseif card3 == 3 then
+    print("diamond")
+  elseif card3 == 4 then
+    print("heart")
+  end
+  if card4 == 1 then
+    print("spade")
+  elseif card4 == 2 then
+    print("club")
+  elseif card4 == 3 then
+    print("diamond")
+  elseif card4 == 4 then
+    print("heart")
+  end
+  if card5 == 1 then
+    print("spade")
+  elseif card5 == 2 then
+    print("club")
+  elseif card5 == 3 then
+    print("diamond")
+  elseif card5 == 4 then
+    print("heart")
+  end
+end
+generateHand()
